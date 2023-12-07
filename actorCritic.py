8e1e3325d630299147545b9dcfd998cbace124b7
Gabriel Leclerc <gabriel.leclerc001@student.uni.lu>
2023-12-07 13:22:17 +0100

    should be good

---
+++ actorCritic.py
@@ -0,0 +1,144 @@
+import tensorflow as tf 
+from tensorflow.keras.optimizers import Adam
+import tensorflow_probability as tfp
+#from networks import actorCriticNetwork
+import numpy as np
+import os
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.models import Model
+import gc
+from tensorflow.keras import backend as K
+class ActorCritic:
+    def __init__(self, alpha=0.01, gamma=0.99, action_space=4, observation_space=8, a1=64, a2=32, c1=64, c2=32, l=1, Async="A"):
+        super(ActorCritic, self).__init__()
+        K.clear_session()
+
+        self.count=0
+        self.countLimit=l
+        self.Async=Async
+
+        self.alpha = alpha
+        self.gamma = gamma
+        self.action_space = action_space
+        self.action = None
+        self.actionSpace = [i for i in range (0, self.action_space)]
+#        self.aLoss = 0
+ #       self.cLoss = 0
+  #      self.aGradient = []
+   #     self.cGradient = []
+
+        self.actor_checkpoint_file = os.path.join("logs/actor")
+        self.critic_checkpoint_file = os.path.join("logs/critic")
+
+        self.aD1_dims = a1
+        self.aD2_dims = a2
+        self.aOutput_dims = action_space
+
+        self.cD1_dims = c1
+        self.cD2_dims = c2
+        self.cOutput_dims = 1
+
+        self.stateInput = Input(shape=(observation_space))
+
+        self.aD1 = Dense(self.aD1_dims, activation=tf.keras.layers.LeakyReLU(), dtype=tf.float64)(self.stateInput)
+        self.aD2 = Dense(self.aD2_dims, activation=tf.keras.layers.LeakyReLU(), dtype=tf.float64)(self.aD1)
+        self.aOutput = Dense(self.aOutput_dims, activation="softmax", dtype=tf.float64)(self.aD2)
+
+        self.cD1 = Dense(self.cD1_dims, activation=tf.keras.layers.LeakyReLU(), dtype=tf.float64)(self.stateInput)
+        self.cD2 = Dense(self.cD2_dims, activation=tf.keras.layers.LeakyReLU(), dtype=tf.float64)(self.cD1)
+        self.cOutput = Dense(self.cOutput_dims, activation=None, dtype=tf.float64)(self.cD2)
+
+        self.Actor = Model(inputs=self.stateInput, outputs=self.aOutput)
+        self.Critic = Model(inputs=self.stateInput, outputs=self.cOutput)
+
+        self.Actor.summary()
+        self.Critic.summary()
+        
+        self.Actor.compile(optimizer=Adam(learning_rate=self.alpha))
+        self.Critic.compile(optimizer=Adam(learning_rate=self.alpha))
+
+    def chooseAction(self, observations):
+        state = tf.convert_to_tensor([observations])
+        proba = self.Actor(state)
+
+        actionProba = tfp.distributions.Categorical(probs=proba)
+        action = actionProba.sample().numpy()[0]
+        self.action = action
+
+        return action, actionProba, proba
+
+    def aSave(self):
+        print("...saving actor...")
+        self.Actor.save_weights(self.actor_checkpoint_file)
+
+    def aLoad(self):
+        print("...loading actor...")
+        self.Actor.load_weights(self.actor_checkpoint_file)
+    
+    def cSave(self):
+        print("...saving critic...")
+        self.Critic.save_weights(self.critic_checkpoint_file)
+
+    def cLoad(self):
+        print("...loading critic...")
+        self.Critic.load_weights(self.critic_checkpoint_file)
+
+    def save(self):
+        print("...saving...")
+        self.aSave()
+        self.cSave()
+
+    def load(self):
+        print("...loading...")
+        self.aLoad()
+        self.cLoad()
+
+    def learn(self, state, reward, state1, done):
+        state = tf.convert_to_tensor([state], dtype=tf.float64)
+        state1 = tf.convert_to_tensor([state1], dtype=tf.float64)
+        reward = tf.convert_to_tensor(reward, dtype=tf.float64)
+        with tf.GradientTape(persistent=True) as tape:
+            tape.reset()
+            actionProba = self.Actor(state)
+
+            V_value = self.Critic(state)
+            V_value1 = self.Critic(state1)
+            V_value = tf.squeeze(V_value)
+            V_value1 = tf.squeeze(V_value1)
+
+            actionProba = tfp.distributions.Categorical(probs=actionProba)
+            logProba = actionProba.log_prob(self.action)
+
+            advantage = reward + self.gamma*V_value1*(1-int(done)) - V_value
+
+            aLoss = -logProba * advantage
+            cLoss = advantage ** 2
+#            self.aLoss = aLoss
+ #           self.cLoss = cLoss
+
+        aGradient = tape.gradient(aLoss, self.Actor.trainable_variables)
+        cGradient = tape.gradient(cLoss, self.Critic.trainable_variables)
+        
+  #      self.aGradient.append(aGradient)
+   #     self.cGradient.append(cGradient)
+        self.count += 1
+        if self.Async == "A":
+            if self.count == self.countLimit:
+                self.Actor.optimizer.apply_gradients(zip(aGradient, self.Actor.trainable_variables))
+                self.count = 0
+            self.Critic.optimizer.apply_gradients(zip(cGradient, self.Critic.trainable_variables))
+        elif self.Async == "C":
+            self.Actor.optimizer.apply_gradients(zip(aGradient, self.Actor.trainable_variables))
+            if self.count == self.countLimit:
+                self.Critic.optimizer.apply_gradients(zip(cGradient, self.Critic.trainable_variables))
+                self.count = 0
+
+        del tape
+        del state
+        del state1
+        del reward
+        del actionProba
+        del V_value
+        del V_value1
+        gc.collect()
---
+++ main.py
@@ -0,0 +1,71 @@
+import gymnasium as gym
+import numpy as np
+from actorCritic import ActorCritic
+from datetime import datetime
+import atexit
+import sys
+import matplotlib.pyplot as plt
+
+def plot_learning_curve(x, scores, figure_file):
+    running_avg = np.zeros(len(scores))
+    for i in range(len(running_avg)):
+        running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
+    plt.plot(x, running_avg)
+    plt.title('Running average of previous 1500 scores')
+    plt.savefig(figure_file)
+
+if __name__ == "__main__":
+    env = gym.make("LunarLander-v2")
+    alpha = input("alpha (1e-4):") or 1e-4
+    gamma = input("gamma (0.99):") or 0.99
+    agent = ActorCritic(alpha=float(alpha), gamma=float(gamma), action_space=env.action_space.n, a1=(input("a1 (86):")) or 86, a2=(input("a2 (32):")) or 32, c1=(input("c1 (86):")) or 86, c2=(input("c2 (32):")) or 32, l=(input("count limit:")) or 1, Async=(input("Async :")) or "A" )
+    n_games = 1500
+    time = datetime.now()
+
+    filePath = "tmp/img/LunarLander"+"-"+str(time)+"-g"+str(agent.gamma)+"-a"+str(agent.alpha)+"-a1_"+str(agent.aD1_dims)+"-a2_"+str(agent.aD2_dims)+"-c1_"+str(agent.cD1_dims)+"-c2_"+str(agent.cD2_dims)+"-Async_"+agent.Async+"-LM_"+str(agent.countLimit)
+    figureFile = filePath
+    highScore = env.reward_range[0]
+    scoreHistory = []
+    loadCheckpoint = False
+    print(filePath+".png")
+
+    if loadCheckpoint:
+        agent.load()
+    for i in range (n_games):
+        print(i)
+        observation = env.reset()[0]
+        done = False
+        score = 0
+        try:
+            while not done:
+                action, actionProba, probs = agent.chooseAction(observation)
+                observation1, reward, done, _, info = env.step(action)
+                score += reward
+                if not loadCheckpoint:
+                    agent.learn(observation, reward, observation1, done)
+                observation = observation1
+        except (KeyboardInterrupt, AssertionError):
+            print(action)
+            print(actionProba.sample())
+            print(probs)
+#            print(agent.aLoss)
+ #           print(agent.cLoss)
+  #          print(agent.aGradient[-10:])
+   #         print(agent.cGradient[-10:])
+            figureFile = figureFile + "-" +str(datetime.now()-time) + ".png"
+            x = [j+1 for j in range (i)]
+            plot_learning_curve(x, scoreHistory, figureFile)
+            sys.exit()
+
+        scoreHistory.append(score)
+        averageScore = np.mean(scoreHistory[-100:])
+
+        if averageScore > highScore:
+            highScore = averageScore
+            if not loadCheckpoint:
+                agent.save()
+        print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % averageScore)
+
+    figureFile = figureFile + "-" + str(datetime.now()-time) + ".png"
+    x = [i+1 for i in range (n_games)]
+    plot_learning_curve(x, scoreHistory, figureFile)
