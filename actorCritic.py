3175da5a8e0f53d72ef2ddd8838d0f00b61b7a39
Gabriel Leclerc <gabriel.leclerc001@student.uni.lu>
2024-01-13 10:22:05 +0100

    Final Version

--- actorCritic.py
+++ actorCritic.py
@@ -1,19 +1,17 @@
 import tensorflow as tf 
 from tensorflow.keras.optimizers import Adam
 import tensorflow_probability as tfp
-#from networks import actorCriticNetwork
 import numpy as np
 import os
 import tensorflow.keras as keras
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Model
-import gc
 from tensorflow.keras import backend as K
 class ActorCritic:
     def __init__(self, alpha=0.01, gamma=0.99, action_space=4, observation_space=8, a1=64, a2=32, c1=64, c2=32, l=1, Async="A", index="0"):
         super(ActorCritic, self).__init__()
         K.clear_session()
-
+        self.index = index
         self.count=0
         self.countLimit=l
         self.Async=Async
@@ -23,13 +21,9 @@ class ActorCritic:
         self.action_space = action_space
         self.action = None
         self.actionSpace = [i for i in range (0, self.action_space)]
-#        self.aLoss = 0
- #       self.cLoss = 0
-  #      self.aGradient = []
-   #     self.cGradient = []
 
-        self.actor_checkpoint_file = os.path.join("logs/"+index+"/actor")
-        self.critic_checkpoint_file = os.path.join("logs/"+index+"/critic")
+        self.actor_checkpoint_file = os.path.join("logs/"+self.index+"/actor")
+        self.critic_checkpoint_file = os.path.join("logs/"+self.index+"/critic")
 
         self.aD1_dims = a1
         self.aD2_dims = a2
@@ -58,47 +52,55 @@ class ActorCritic:
         self.Actor.compile(optimizer=Adam(learning_rate=self.alpha))
         self.Critic.compile(optimizer=Adam(learning_rate=self.alpha))
 
+
     def chooseAction(self, observations):
         state = tf.convert_to_tensor([observations])
         proba = self.Actor(state)
 
         actionProba = tfp.distributions.Categorical(probs=proba)
-        action = actionProba.sample().numpy()[0]
-        self.action = action
+        self.action = actionProba.sample().numpy()[0]
 
-        return action, actionProba, proba
+        return self.action, actionProba, proba
 
     def aSave(self):
         print("...saving actor...")
         self.Actor.save_weights(self.actor_checkpoint_file)
 
-    def aLoad(self):
+    def aLoad(self, logDate):
         print("...loading actor...")
-        self.Actor.load_weights(self.actor_checkpoint_file)
+        if (logDate != None):
+            self.Actor.load_weights(os.path.join("logs_backup_"+logDate+"/"+self.index+"/actor"))
+        else:
+            self.Actor.load_weights(self.actor_checkpoint_file)
     
     def cSave(self):
         print("...saving critic...")
         self.Critic.save_weights(self.critic_checkpoint_file)
 
-    def cLoad(self):
+    def cLoad(self, logDate):
         print("...loading critic...")
-        self.Critic.load_weights(self.critic_checkpoint_file)
+        if (logDate != None):
+            self.Critic.load_weights(os.path.join("logs_backup_"+logDate+"/"+self.index+"/critic"))
+        else:
+            self.Critic.load_weights(self.critic_checkpoint_file)
 
     def save(self):
         print("...saving...")
         self.aSave()
         self.cSave()
 
-    def load(self):
+    def load(self, logDate):
         print("...loading...")
-        self.aLoad()
-        self.cLoad()
+        self.aLoad(logDate)
+        self.cLoad(logDate)
 
     def learn(self, state, reward, state1, done):
         state = tf.convert_to_tensor([state], dtype=tf.float64)
         state1 = tf.convert_to_tensor([state1], dtype=tf.float64)
         reward = tf.convert_to_tensor(reward, dtype=tf.float64)
+        
         with tf.GradientTape(persistent=True) as tape:
+            
             actionProba = self.Actor(state)
 
             V_value = self.Critic(state)
@@ -113,14 +115,10 @@ class ActorCritic:
 
             aLoss = -logProba * advantage
             cLoss = advantage ** 2
-#            self.aLoss = aLoss
- #           self.cLoss = cLoss
 
         aGradient = tape.gradient(aLoss, self.Actor.trainable_variables)
         cGradient = tape.gradient(cLoss, self.Critic.trainable_variables)
         
-  #      self.aGradient.append(aGradient)
-   #     self.cGradient.append(cGradient)
         self.count += 1
         if self.Async == "A":
             if self.count == self.countLimit:
@@ -134,4 +132,10 @@ class ActorCritic:
                 self.count = 0
 
         del tape
-        gc.collect()
+        del state
+        del state1
+        del reward
+        del V_value
+        del V_value1
+        del aGradient
+        del cGradient
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import gymnasium as gym
 import numpy as np
 from actorCritic import ActorCritic
 from datetime import datetime
-import atexit
 import sys
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
 
 def plot_learning_curve(x, scores, figure_file):
     running_avg = np.zeros(len(scores))
@@ -24,14 +26,15 @@ if __name__ == "__main__":
     time = datetime.now()
 
     filePath = "tmp/img"+index+"/LunarLander"+"-"+str(time)+"-g"+str(agent.gamma)+"-a"+str(agent.alpha)+"-a1_"+str(agent.aD1_dims)+"-a2_"+str(agent.aD2_dims)+"-c1_"+str(agent.cD1_dims)+"-c2_"+str(agent.cD2_dims)+"-Async_"+agent.Async+"-LM_"+str(agent.countLimit)
-    figureFile = filePath
     highScore = env.reward_range[0]
     scoreHistory = []
-    loadCheckpoint = False
+    loadCheckpoint = True
     print(filePath+".png")
 
     if loadCheckpoint:
-        agent.load()
+        logDate = input("log date:") or None
+        agent.load(logDate)
+        n_games = 501
     for i in range (n_games):
         print(i)
         observation = env.reset()[0]
@@ -44,20 +47,28 @@ if __name__ == "__main__":
                 score += reward
                 if not loadCheckpoint:
                     agent.learn(observation, reward, observation1, done)
+                elif score <= -500
+                    done = True
                 observation = observation1
         except (KeyboardInterrupt, AssertionError):
             print(action)
             print(actionProba.sample())
             print(probs)
-#            print(agent.aLoss)
- #           print(agent.cLoss)
-  #          print(agent.aGradient[-10:])
-   #         print(agent.cGradient[-10:])
-            figureFile = figureFile + "-" +str(datetime.now()-time) + ".png"
+           
+            now = str(datetime.now()-time)
+            figureFile = filePath + "-" + now + ".png"
             x = [j+1 for j in range (i)]
             plot_learning_curve(x, scoreHistory, figureFile)
-            sys.exit()
 
+            if loadCheckpoint:
+                figureFile1 = filePath + "-" + now + "training" + ".png"
+                dataset = pd.DataFrame({'Reward': scoreHistory})
+                dataset['Succeded'] = dataset.Reward.apply(lambda x: True if x > 200 else False)
+                plot = sns.displot(x="Reward", data=dataset)
+                plot.savefig(figureFile1)
+
+            sys.exit()
+        
         scoreHistory.append(score)
         averageScore = np.mean(scoreHistory[-100:])
 
@@ -67,6 +78,18 @@ if __name__ == "__main__":
                 agent.save()
         print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % averageScore)
 
-    figureFile = figureFile + "-" + str(datetime.now()-time) + ".png"
+    now = str(datetime.now()-time)
+    figureFile = filePath + "-" + now + ".png"
     x = [i+1 for i in range (n_games)]
     plot_learning_curve(x, scoreHistory, figureFile)
+
+
+    if loadCheckpoint:
+        figureFile1 = filePath + "-" + now + "training" + ".png"
+        dataset = pd.DataFrame({'Reward': scoreHistory})
+        dataset['Succeded'] = dataset.Reward.apply(lambda x: True if x > 200 else False)
+        plot = sns.displot(x="Reward", data=dataset)
+        plot.savefig(figureFile1)
+
+
+
